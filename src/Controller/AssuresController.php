7a7f9a71632b0f2aebddd96f1cbc0f64e0aa9f56
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2026-06-22 15:21:17 +0300

    Rajout du fichier login et index

--- src/Controller/AssuresController.php
+++ src/Controller/AssuresController.php
@@ -12,28 +12,22 @@ final class AssuresController extends AbstractController
 {
     
      // Route pour la page de login
-    #[Route('/login', name: 'app_assures_login')]
-    public function index(Request $request): Response
+    #[Route('/dashboard', name: 'app_assures_login')]
+    public function index(): Response
     {
-        
-        // Récupération des paramètres GET nom et prenom avec des valeurs par défaut
         $nom = "moussa";
         $prenom = "ali";
         $role = "admin";
        
-
-        //Retourne une réponse 
-         /*return $this->render('premier_symfony/index.html.twig', [
+        return $this->render('premier_symfony/index.html.twig', [
              'nom' => $nom,
              'prenom' => $prenom,                                                                                                                             
              'role' => $role
-         ]); */
-
-         return new Response();
+         ]);
     }
 
     // Route pour la page du tableau de bord
-    #[Route('/dashboard', name: 'dashBoard')]
+    #[Route('/login', name: 'dashBoard')]
     public function dashBord(): Response
     {
        // return $this->render('dashboard/index.html.twig');
