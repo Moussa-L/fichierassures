62117aaa30d61b1ae7c920d65784367e97caa0ac
Moussa-L <moussalidya07@gmail.com>
2026-06-22 15:43:16 +0400

    Création des routes faite

--- src/Controller/AssuresController.php
+++ src/Controller/AssuresController.php
@@ -5,15 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class AssuresController extends AbstractController
 {
-    #[Route('/assures', name: 'app_assures')]
-    public function index(): JsonResponse
+    
+     // Route pour la page de login
+    #[Route('/login', name: 'app_assures_login')]
+    public function index(Request $request): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AssuresController.php',
-        ]);
+        
+        // Récupération des paramètres GET nom et prenom avec des valeurs par défaut
+        $nom = "moussa";
+        $prenom = "ali";
+        $role = "admin";
+       
+
+        //Retourne une réponse 
+         /*return $this->render('premier_symfony/index.html.twig', [
+             'nom' => $nom,
+             'prenom' => $prenom,                                                                                                                             
+             'role' => $role
+         ]); */
+
+         return new Response();
     }
+
+    // Route pour la page du tableau de bord
+    #[Route('/dashboard', name: 'dashBoard')]
+    public function dashBord(): Response
+    {
+       // return $this->render('dashboard/index.html.twig');
+       return new Response();
+    }       
 }
