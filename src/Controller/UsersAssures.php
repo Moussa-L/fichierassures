1ac9aac0a83b4dc948cb14ef889932b123f80c15
Moussa-L <moussalidya07@gmail.com>
2026-07-01 12:53:22 +0400

    ajout des utilisateur

--- src/Controller/UsersAssures.php
+++ src/Controller/UsersAssures.php
@@ -1,26 +1,9 @@
 <?php
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$users = new Users("J00567", "HedjaZaharir", "12345");
+$users = new Users("J00987", "SouffouDine", "09864");
+$users = new Users("J00453", "MohamedaAhmed", "56478");
+$users = new Users("J00637", "SouffouEchat", "25806");
 
 
 
