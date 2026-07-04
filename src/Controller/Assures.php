733c917dc15b8b2b050e1b2e43a7f289117e7a9d
Moussa-L <moussalidya07@gmail.com>
2026-07-04 18:05:08 +0400

    assures

--- src/Controller/Assures.php
+++ src/Controller/Assures.php
@@ -1,8 +1,7 @@
 <?php
 // je vais créer les Assures dans un tableau pour les tester
 
-$assures1 = new Coordonnee("", "HedjaZaharir", "12345");
-
-
+$assures1 = new Coordonnee("2901199397081", "1250497612003", "MADI", "IRCHAM MOURSAL", "14/04/2025", "(vide)", "24/02/2027", "BDO", "", "");
+		
 
 ?>
