21ee814f81cd7cdcbd46d69e33550acef76b245f
Moussa-L <moussalidya07@gmail.com>
2026-07-06 09:12:20 +0400

    ajout de nouvelle colone dans le tab

--- src/Controller/Assures.php
+++ src/Controller/Assures.php
@@ -8,13 +8,17 @@ final class Assures
     {
         return [[
             'nir' => '2901199397081',
-            'numCompostage' => '1250497612003',
+            'nirBnf' => '1250497612003',
             'nom' => 'MADI',
             'prenom' => 'IRCHAM MOURSAL',
             'dateTraitement' => new \DateTime('2025-04-14'),
             'lieuNaissance' => 'MARRAKECH',
             'dateNaissance' => new \DateTime('1993-01-29'),
-            'nirBnf' => '1250497612003',
+            'adresse' => '123 Rue de la Liberté',
+            'addresseComplete' => '123 Rue de la Liberté, Marrakech',
+            'dateFinJOD' => new \DateTime('2025-04-14'),
+            'source' => 'Source A',
+            
         ]];
     }
 }
