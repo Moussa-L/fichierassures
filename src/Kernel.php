27b390b9d08c66882dd6bd1c3d59d494a296b703
Moussa-L <moussalidya07@gmail.com>
2026-07-08 11:47:20 +0400

    ajout de l'image

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,9 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+// Classe principale du noyau de l'application Symfony.
+// Elle hérite du Kernel de Symfony et utilise le trait MicroKernelTrait
+// pour gérer la configuration minimale et les routes de manière moderne.
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
