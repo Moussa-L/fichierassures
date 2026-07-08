ee7328070fc10b7ac5b91d8f92b507f71d5726ac
Moussa-L <moussalidya07@gmail.com>
2026-07-08 09:32:14 +0400

    modificationd index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,11 @@
 
 use App\Kernel;
 
+// Charge l'autoloader de Symfony pour pouvoir utiliser les classes de l'application.
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// Point d'entrée de l'application Symfony.
+// Crée une instance du noyau avec l'environnement et le mode debug fournis par le contexte.
 return static function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
