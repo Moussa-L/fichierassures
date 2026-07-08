9768c94f76377ed5054ec130a7bb38b02af75855
Moussa-L <moussalidya07@gmail.com>
2026-07-08 14:17:40 +0400

    toujour avec les routes

--- src/Controller/AssuresController.php
+++ src/Controller/AssuresController.php
@@ -3,9 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Users;
-use App\Entity\Coordonnee;
-use App\Repository\UsersRepository;
-use App\Repository\CoordonneeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,29 +10,28 @@ use Symfony\Component\HttpFoundation\Response;
 final class AssuresController extends AbstractController
 {
     #[Route('/login', name: 'app_assures_login')]
-    public function index(UsersRepository $usersRepository): Response
+    public function index(): Response
     {
-        // Récupérer tous les utilisateurs depuis la BD
-        $users = $usersRepository->findAll();
+        
+
+        
 
         return $this->render('premier_symfony/login.html.twig', [
-            'users' => $users,
+            
+            //'LesUsers' => $LesUsers,
         ]);
     }
 
     // Route pour la page du tableau de bord
     #[Route('/dashboard', name: 'dashBoard')]
-    public function dashBord(CoordonneeRepository $coordonneeRepository, UsersRepository $usersRepository): Response
+    public function dashBord(): Response
     {
-        // Récupérer tous les assurés depuis la BD
-        $assures = $coordonneeRepository->findAll();
-        
-        // Récupérer tous les utilisateurs depuis la BD
-        $users = $usersRepository->findAll();
+        $assures = Assures::getTestData();
+        require __DIR__ . '/UsersAssures.php';
 
         return $this->render('premier_symfony/dashboard.html.twig', [
             'Assures' => $assures,
-            'user' => $users,
+            'user' => $users1,
         ]);
     }
 }
--- src/Entity/CoordonneeAssures.php
+++ src/Entity/CoordonneeAssures.php
@@ -1,47 +1,22 @@
 <?php
 
+
 namespace App\Entity;
 
 use App\Repository\CoordonneeRepository;
-use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: CoordonneeRepository::class)]
-#[ORM\Table(name: 'coordonnees_assures')]
+
 class Coordonnee
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private ?int $id = null;
-
-    #[ORM\Column(type: 'string', length: 50)]
     private ?string $nirAss = null;
-
-    #[ORM\Column(type: 'string', length: 50)]
     private ?string $nirBnf = null;
-
-    #[ORM\Column(type: 'string', length: 255)]
     private ?string $nom = null;
-
-    #[ORM\Column(type: 'string', length: 255)]
     private ?string $prenom = null;
-
-    #[ORM\Column(type: 'datetime', nullable: true)]
-    private ?\DateTime $dateNaissance = null;
-
-    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $dateNaissance = null;
     private ?string $lieuNaissance = null;
-
-    #[ORM\Column(type: 'string', length: 500)]
     private ?string $addresse = null;
-
-    #[ORM\Column(type: 'string', length: 500)]
     private ?string $addresseComplete = null;         
-
-    #[ORM\Column(type: 'datetime', nullable: true)]
-    private ?\DateTime $dateFinJOD = null;
-
-    #[ORM\Column(type: 'string', length: 255)]
+    private ?string $dateFinJOD = null;
     private ?string $source;
 
 
